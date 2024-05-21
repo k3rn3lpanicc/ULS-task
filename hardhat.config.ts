8da499278e91874dc5136e7f7eddbfbd61db4e71
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-05-21 22:02:40 +0330

    Add sepolia to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+    networks: {
+        sepolia: {
+            url: 'https://sepolia.infura.io/v3/' + (process.env.PID as string),
+            chainId: 11155111,
+            accounts: [process.env.PRIVATE_KEY as string],
+        },
+    },
+    solidity: '0.8.24',
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.SCANAPI as string,
+        },
+    },
 };
 
 export default config;
