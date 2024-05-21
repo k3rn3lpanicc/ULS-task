a202e8a3c755bc69c4b0cf92eccdf6c583f6e3fa
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-05-21 22:01:38 +0330

    Add backend part

---
+++ backend/app.ts
@@ -0,0 +1,53 @@
+import { abi } from 'abi.js';
+import { configDotenv } from 'dotenv';
+import {ethers} from 'ethers';
+import {writeFile} from 'node:fs';
+configDotenv();
+
+type MetadataChange = {
+    parent: string,
+    child: string,
+    childIdentifier: string,
+    parentIdentifier: string
+}
+
+const _provider = new ethers.providers.WebSocketProvider(
+    `wss://sepolia.infura.io/ws/v3/${process.env.api}`
+);
+
+const provider = new ethers.Wallet(process.env.pk, _provider);
+const deployedAddress = '0x4319Da6D5641f93c607e6f903cD00cB55773E1bC';
+const ulsOperator = new ethers.Contract(deployedAddress, abi, provider);
+ulsOperator.on('AddChild', metadataChanged);
+
+let changeBuffer: MetadataChange[] = [];
+
+function metadataChanged(parent: string, child: string, childIdentifier: string, parentIdentifier: string){
+    console.log(`+ Metadata change:\nParent: ${parent}\nChild: ${child}\nChildIdentifier: ${childIdentifier}\nParentIdentifier: ${parentIdentifier}`)
+    changeBuffer.push({parent, child, childIdentifier, parentIdentifier});
+}
+
+function updateMetadatas(){
+    console.log(`* Updating metadatas...`)
+    for(let i = 0; i < changeBuffer.length; i++){
+        const change = changeBuffer[i];
+        applyChange(change);
+    }
+    console.log(`* Updated metadatas\n`);
+    changeBuffer = [];
+}
+
+function applyChange(change: MetadataChange){
+    const toWrite = "salam";
+    writeFile(
+        `./metadatas/${
+            change.parentIdentifier.split('/')[
+                change.parentIdentifier.split('/').length - 1
+            ]
+        }`,
+        toWrite, (data) => console.log
+    );
+}
+
+setInterval(updateMetadatas, 30000);
+console.log(`app started...`)
